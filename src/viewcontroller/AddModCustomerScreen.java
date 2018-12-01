1b570c1986fa1a92b8e9805f69a7c3e985418324
ScoJoh <wscottjohnson75@gmail.com>
2018-11-30 20:43:52 -0500

    Last Commit excluded the new class and screen file.

---
+++ src/viewcontroller/AddModCustomerScreen.java
@@ -0,0 +1,40 @@
+package viewcontroller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class AddModCustomerScreen {
+
+    @FXML
+    private Button addModButton;
+
+    @FXML
+    private Label addModLabel;
+
+    @FXML
+    private TextField cityField;
+
+    @FXML
+    private TextField customerNameField;
+
+    @FXML
+    private TextField postalCodeField;
+
+    @FXML
+    private TextField address2Field;
+
+    @FXML
+    private TextField address1Field;
+
+    @FXML
+    private TextField countryField;
+
+    @FXML
+    void addModCustomerHandler(ActionEvent event) {
+
+    }
+
+}
