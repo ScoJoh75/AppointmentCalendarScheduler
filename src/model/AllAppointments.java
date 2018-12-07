9f4f93d7db21fdaf3369ea6578386b57f51f65b7
ScoJoh <wscottjohnson75@gmail.com>
2018-12-06 21:50:42 -0500

    Completes the Appointment class and adds the AllAppointments class.

---
+++ src/model/AllAppointments.java
@@ -0,0 +1,33 @@
+package model;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class AllAppointments {
+    private ObservableList<Appointment> allAppointments = FXCollections.observableArrayList();
+
+    public void addAppointment(Appointment appointment) {
+        this.allAppointments.add(appointment);
+    } // end addAppointment
+
+    public boolean removeAppointment(Appointment appointment) {
+        boolean removed = false;
+        if(allAppointments.remove(appointment)) {
+            removed = true;
+        } // end if
+        return removed;
+    } // end removeAppointment
+
+    public Appointment getAppointment(int appointmentId) {
+        return allAppointments.get(appointmentId);
+    } // end getAppointment
+
+    public ObservableList<Appointment> getAllAppointments() {
+        return allAppointments;
+    } // end getAllCustomers
+
+    public void updateAppointment(Appointment appointment, int index) {
+        allAppointments.set(index, appointment);
+    } // end updateAppointment
+
+} // end AllAppointments
--- src/model/Appointment.java
+++ src/model/Appointment.java
@@ -26,4 +26,87 @@ public class Appointment {
         this.endTime = startTime.plusHours(apptLength);
     } // end constructor
 
+    public Appointment(int Id, int customerId, int consultantId, String title, String description, String location, String type, ZonedDateTime startTime, int apptLength) {
+        this.Id = Id;
+        this.customerId = customerId;
+        this.consultantId = consultantId;
+        this.title = title;
+        this.description = description;
+        this.location = location;
+        this.type = type;
+        this.startTime = startTime;
+        this.endTime = startTime.plusHours(apptLength);
+    } // end constructor
+
+    public int getId() {
+        return Id;
+    } // end getId
+
+    public void setId(int id) {
+        Id = id;
+    } // end setId
+
+    public int getCustomerId() {
+        return customerId;
+    } // end getCustomerId
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    } // end setCustomerId
+
+    public int getConsultantId() {
+        return consultantId;
+    } // end getConsultantId
+
+    public void setConsultantId(int consultantId) {
+        this.consultantId = consultantId;
+    } // end setConsultantId
+
+    public String getTitle() {
+        return title;
+    } // end getTitle
+
+    public void setTitle(String title) {
+        this.title = title;
+    } // end setTitle
+
+    public String getDescription() {
+        return description;
+    } // end getDescription
+
+    public void setDescription(String description) {
+        this.description = description;
+    } // end setDescription
+
+    public String getLocation() {
+        return location;
+    } // end getLocation
+
+    public void setLocation(String location) {
+        this.location = location;
+    } // end setLocation
+
+    public String getType() {
+        return type;
+    } // end getType
+
+    public void setType(String type) {
+        this.type = type;
+    } // end setType
+
+    public ZonedDateTime getStartTime() {
+        return startTime;
+    } // end getStartTime
+
+    public void setStartTime(ZonedDateTime startTime) {
+        this.startTime = startTime;
+    } // end setStartTime
+
+    public ZonedDateTime getEndTime() {
+        return endTime;
+    } // end getEndTime
+
+    public void setEndTime(ZonedDateTime endTime) {
+        this.endTime = endTime;
+    } // end setEndTime
 } // end Appointment
